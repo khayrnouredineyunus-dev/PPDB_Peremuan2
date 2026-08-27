d9e4a67c7e58d878d22ed69e22fba72a5b81fe94
khayrnouredineyunus-dev <khayrnouredineyunus@mail.ugm.ac.id>
2026-08-27 10:04:19 +0700

    Last commit

--- app/src/main/java/com/example/countapp/MainActivity.kt
+++ app/src/main/java/com/example/countapp/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.countapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.countapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var number = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            txtNumber.text = number.toString()
+            btnCount.setOnClickListener {
+                number++
+                txtNumber.text = number.toString()
+            }
+            btnToast.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Count $number",
+                    Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
